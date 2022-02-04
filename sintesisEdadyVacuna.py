4133ae3dcdaa8a4cd54211118d9e4003feb35183
pescap <paulescapil-inchauspe@MacBook-Air-de-Paul.local>
2022-02-04 18:23:47 -0300

    added heatmaps

--- sintesisEdadyVacuna.py
+++ sintesisEdadyVacuna.py
@@ -31,16 +31,12 @@ df12["p_casos_confirmados"] = df12.poblacion / df12.casos_confirmados
 df12["p_casos_uci"] = df12.poblacion / df12.casos_uci
 
 df12["p_casos_def"] = df12.poblacion / df12.casos_def
-
-df12["p_casos_confirmados"] = df12["p_casos_confirmados"]
-df12["p_casos_uci"] = df12["p_casos_uci"]
-df12["p_casos_def"] = df12["p_casos_def"]
-
 df12["p_casos_uci_str"] = df12["p_casos_uci"].astype('str')
 
 #df12.estado_vacunacion = df12.estado_vacunacion.astype('category')
 
-pivot_confirmados = df12.pivot_table(values = "p_casos_confirmados", columns = "estado_vacunacion", index = "grupo_edad").astype(int)
+pivot_poblacion = df12.pivot_table(values = "poblacion", columns = "estado_vacunacion", index = "grupo_edad")
+pivot_confirmados = df12.pivot_table(values = "p_casos_confirmados", columns = "estado_vacunacion", index = "grupo_edad")
 pivot_uci = df12.pivot_table(values = "p_casos_uci", columns = "estado_vacunacion", index = "grupo_edad")
 pivot_def = df12.pivot_table(values = "p_casos_def", columns = "estado_vacunacion", index = "grupo_edad")
 
@@ -48,20 +44,55 @@ cols = ['sin esquema completo',
  'con esquema completo',
  'con dosis refuerzo > 14 dias']
 
-pivot_confirmados = pivot_confirmados[cols]
-pivot_uci = pivot_uci[cols]
-pivot_def = pivot_def[cols]
+
+pivot_poblacion = pivot_poblacion.values
+pivot_confirmados = pivot_confirmados[cols].values
+pivot_uci = pivot_uci[cols].values
+pivot_def = pivot_def[cols].values
+
+vals_uci = np.zeros(pivot_uci.shape, dtype = int)
+vals_def = np.zeros(pivot_def.shape, dtype = int)
+
+mask_uci = np.zeros(pivot_uci.shape, dtype = bool)
+mask_def = np.zeros(pivot_def.shape, dtype = bool)
+
+pivot_uci_str = np.zeros(pivot_uci.shape, dtype=object)
+pivot_def_str = np.zeros(pivot_def.shape, dtype=object)
+
+for i in range(pivot_uci_str.shape[0]):
+    for j in range(pivot_uci_str.shape[1]):
+
+        if not np.isinf(pivot_uci[i, j]):
+            mask_uci[i, j] = False
+            vals_uci[i, j] = int(pivot_uci[i, j])
+            pivot_uci_str[i, j] = str(vals_uci[i, j])            
+        else:
+            mask_uci[i, j] = True
+            vals_uci[i, j] = int(pivot_poblacion[i, j])
+            pivot_uci_str[i, j] = '>\n' + str(vals_uci[i, j])
+            
+        if not np.isinf(pivot_def[i, j]):
+            mask_def[i, j] = False
+            vals_def[i, j] = int(pivot_def[i, j])
+            pivot_def_str[i, j] = str(vals_def[i, j])            
+        else:
+            mask_def[i, j] = True
+            vals_def[i, j] = int(pivot_poblacion[i, j])
+            pivot_def_str[i, j] = '>\n' + str(vals_uci[i, j])
 
 
 import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 
-fig, (ax1, ax2, ax3) = plt.subplots(1,3)
+fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize = (40,8))
 
 sns.set(font="Arial")
-g = sns.heatmap(data = pivot_confirmados , cmap = 'RdYlGn', annot = True, square = True, ax = ax1, fmt= 'd')#, cbar = None)
-g = sns.heatmap(data = pivot_uci , cmap = 'RdYlGn', annot = True, square = True, ax = ax2)#, cbar = None)
-g = sns.heatmap(data = pivot_def , cmap = 'RdYlGn', annot = True, square = True, ax = ax3)#, cbar = None)
+g = sns.heatmap(data = pivot_confirmados , cmap = 'RdYlGn', annot = True, square = True, ax = ax1, fmt= '.0f', cbar = None)
+g = sns.heatmap(data = vals_uci , cmap = 'RdYlGn', annot = pivot_uci_str, square = True, ax = ax2, cbar = None, fmt = '', mask = mask_uci)
+g = sns.heatmap(data = np.ones(vals_uci.shape), cmap = 'Blues', annot = pivot_uci_str, square = True, ax = ax2, cbar = None, fmt = '', mask = ~mask_uci, color = 'g')
+
+g = sns.heatmap(data = vals_def , cmap = 'RdYlGn', annot = pivot_def_str, square = True, ax = ax3, fmt= '', cbar = None, mask = mask_def)
+g = sns.heatmap(data = np.ones(vals_def.shape), cmap='Blues', annot = pivot_def_str, square = True, ax = ax3, cbar = None, fmt = '', mask = ~mask_def, color = 'g')
 plt.show(block=False)
 
 
